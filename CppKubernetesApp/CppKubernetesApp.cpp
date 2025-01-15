664aea7374ed6511f88eb74d2e5646477c60875c
taevin.bellamy@machiningcloud.com <taevin.bellamy@machiningcloud.com>
2025-01-15 11:35:44 -0800

    fix config files and local env for dev

--- CppKubernetesApp/CppKubernetesApp.cpp
+++ CppKubernetesApp/CppKubernetesApp.cpp
@@ -1,46 +1,71 @@
+
+#include "targetver.h"
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/asio.hpp>
 #include <iostream>
+#include <memory>
+#include <thread>
+
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace net = boost::asio;
+using tcp = net::ip::tcp;
+
+void handle_request(http::request<http::string_body> req, std::shared_ptr<tcp::socket> socket) {
+    // Create a response
+    http::response<http::string_body> res{ http::status::ok, req.version() };
+    res.set(http::field::server, "Boost.Beast");
+    res.set(http::field::content_type, "text/plain");
+    res.body() = "Hello, Kubernetes from Visual Studio!";
+    res.prepare_payload();
+
+    // Write the response
+    http::write(*socket, res);
+}
+
+void do_session(std::shared_ptr<tcp::socket> socket) {
+    try {
+        beast::flat_buffer buffer;
+        http::request<http::string_body> req;
+
+        // Read the request
+        http::read(*socket, buffer, req);
+
+        // Handle the request
+        handle_request(std::move(req), socket);
+
+        // Close the socket
+        beast::error_code ec;
+        socket->shutdown(tcp::socket::shutdown_send, ec);
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Session error: " << e.what() << std::endl;
+    }
+}
+
+void do_accept(tcp::acceptor& acceptor, net::io_context& ioc) {
+    acceptor.async_accept([&](beast::error_code ec, tcp::socket socket) {
+        if (!ec) {
+            std::make_shared<std::thread>([s = std::make_shared<tcp::socket>(std::move(socket))]() {
+                do_session(s);
+                })->detach();
+        }
+        do_accept(acceptor, ioc);
+        });
+}
 
 int main() {
     try {
-        boost::asio::io_context ioc;
+        net::io_context ioc{ 1 };
+        tcp::acceptor acceptor{ ioc, {tcp::v4(), 8080} };
 
-        // Create an HTTP server
-        boost::asio::ip::tcp::acceptor acceptor(ioc, {boost::asio::ip::tcp::v4(), 8080});
-        boost::asio::ip::tcp::socket socket(ioc);
+        do_accept(acceptor, ioc);
 
         std::cout << "Server is listening on port 8080...\n";
-
-        boost::system::error_code ec;
-        while (true) {
-    try {
-        acceptor.accept(socket, ec);
-        if (ec) {
-            std::cerr << "Error accepting connection: " << ec.message() << "\n";
-            return 1;
-        }      
-        }
-        catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
+        ioc.run();
     }
-} 
-        boost::beast::flat_buffer buffer;
-
-        boost::beast::http::request<boost::beast::http::string_body> request;
-        boost::beast::http::read(socket, buffer, request);
-
-        boost::beast::http::response<boost::beast::http::string_body> response{
-            boost::beast::http::status::ok, request.version()};
-        response.set(boost::beast::http::field::server, "Boost.Beast");
-        response.set(boost::beast::http::field::content_type, "text/plain");
-        response.body() = "Hello, Kubernetes from Visual Studio!";
-        response.prepare_payload();
-
-        boost::beast::http::write(socket, response);
-        socket.shutdown(boost::asio::ip::tcp::socket::shutdown_send);
-    } catch (const std::exception& e) {
+    catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
     }
     return 0;
